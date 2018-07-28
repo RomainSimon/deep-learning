782758bf4d0f1ebb3b6f6b8377935f622de73b6c
Romain SIMON <contact@romainsimon.net>
2018-07-28 16:46:13 +0200

    Fix: neural network

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Deep Learning - Udemy course
 
+[Course link](https://www.udemy.com/le-deep-learning-de-a-a-z)
+
 ## ANN
 
 Simple churn prediction using Tensorflow
--- ann.py
+++ ann.py
@@ -8,22 +8,23 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('./datasets/Churn_Modelling.csv')
-X = dataset.iloc[:, [3, 13]].values
+X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_Geo = LabelEncoder()
-X[:, 1] = labelencoder_X_Geo.fit_transform(X[:, 1])
-labelencoder_X_Gender = LabelEncoder()
-X[:, 2] = labelencoder_X_Gender.fit_transform(X[:, 2])
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
 
 # Splitting dataset into training and test set
-from sklearn.mode_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -59,3 +60,11 @@ classifier.compile(optimizer='adam', loss='binary_crossentropy',
 
 # Training ANN
 classifier.fit(X_train, y_train, batch_size=10, epochs=100)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
